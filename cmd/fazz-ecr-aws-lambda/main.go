f69a7ee47a74b0e40b06d4b6e2799dfa07e78d9a
Kurnia D Win <kurnia.d.win@gmail.com>
2021-05-19 16:22:55 +0700

    update

--- cmd/fazz-ecr-aws-lambda/main.go
+++ cmd/fazz-ecr-aws-lambda/main.go
@@ -21,53 +21,57 @@ func main() {
 type h struct{}
 
 func (h) Invoke(ctx context.Context, payload []byte) ([]byte, error) {
-	f := func() ([]byte, error) {
-		var input events.APIGatewayV2HTTPRequest
-		if err := json.Unmarshal(payload, &input); err != nil {
-			return nil, errors.Trace(err)
-		}
+	var ret []byte
+	var err error
 
-		claims := input.RequestContext.Authorizer.JWT.Claims
-		if claims["iss"] != oidcconfig.Issuer || claims["aud"] != oidcconfig.ClientID {
-			return resp401(), nil
-		}
+	err = errors.Catch(func() error {
+		ret, err = func() ([]byte, error) {
+			var input events.APIGatewayV2HTTPRequest
+			if err := json.Unmarshal(payload, &input); err != nil {
+				return nil, errors.Trace(err)
+			}
 
-		// it is safe to trust content of jwt token, because api gateway already verify it for us
+			claims := input.RequestContext.Authorizer.JWT.Claims
+			if claims["iss"] != oidcconfig.Issuer || claims["aud"] != oidcconfig.ClientID {
+				return resp401(), nil
+			}
 
-		authParts := strings.Split(strings.TrimPrefix(input.Headers["authorization"], "Bearer "), ".")
-		jwtBodyRaw, err := base64.RawURLEncoding.DecodeString(authParts[1])
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
+			// it is safe to trust content of jwt token, because api gateway already verify it for us
 
-		var jwtBody struct {
-			Email  string   `json:"email"`
-			Groups []string `json:"groups"`
-		}
-		if err := json.Unmarshal(jwtBodyRaw, &jwtBody); err != nil {
-			return nil, errors.Trace(err)
-		}
-		if jwtBody.Email == "" {
-			return resp400("cannot accept jwt token without email"), nil
-		}
-
-		if input.RouteKey == "GET /docker-login" {
-			cred, err := exchangesvc.GetCredFor(jwtBody.Email, jwtBody.Groups)
+			authParts := strings.Split(strings.TrimPrefix(input.Headers["authorization"], "Bearer "), ".")
+			jwtBodyRaw, err := base64.RawURLEncoding.DecodeString(authParts[1])
 			if err != nil {
 				return nil, errors.Trace(err)
 			}
 
-			return respCred(cred), nil
-		}
+			var jwtBody struct {
+				Email  string   `json:"email"`
+				Groups []string `json:"groups"`
+			}
+			if err := json.Unmarshal(jwtBodyRaw, &jwtBody); err != nil {
+				return nil, errors.Trace(err)
+			}
+			if jwtBody.Email == "" {
+				return resp400("cannot accept jwt token without email"), nil
+			}
 
-		return resp404(), nil
-	}
+			if input.RouteKey == "GET /docker-login" {
+				cred, err := exchangesvc.GetCredFor(jwtBody.Email, jwtBody.Groups)
+				if err != nil {
+					return nil, errors.Trace(err)
+				}
 
-	var ret []byte
-	var err error
-	err = errors.Catch(func() error { ret, err = f(); return err })
+				return respCred(cred), nil
+			}
+
+			return resp404(), nil
+		}()
+		return err
+	})
+
 	if err != nil {
 		return resp500(err), nil
 	}
+
 	return ret, nil
 }
